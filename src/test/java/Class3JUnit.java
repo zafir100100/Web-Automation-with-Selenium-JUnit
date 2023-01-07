987aca33d0241eb7c6a41fe17432abf38758a73b
Zafir <55599023+zafir100100@users.noreply.github.com>
2023-01-07 16:04:40 +0600

    Update Class3JUnit.java

--- src/test/java/Class3JUnit.java
+++ src/test/java/Class3JUnit.java
@@ -4,11 +4,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Class3JUnit {
 
@@ -38,6 +42,60 @@ public class Class3JUnit {
         driver.close();
         driver.switchTo().window(w.get(0));
     }
+
+    @Test
+    public void handleMultipleWindows() throws InterruptedException {
+        driver.get("https://demoqa.com/browser-windows");
+        //Thread.sleep(5000);
+        // WebDriverWait wait = new WebDriverWait(driver, 30);
+        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("windowButton")));
+        driver.findElement(By.id(("windowButton"))).click();
+        String mainWindowHandle = driver.getWindowHandle();
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        Iterator<String> iterator = allWindowHandles.iterator();
+        while (iterator.hasNext()) {
+            String ChildWindow = iterator.next();
+            if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
+                driver.switchTo().window(ChildWindow);
+                String text = driver.findElement(By.id("sampleHeading")).getText();
+                Assert.assertTrue(text.contains("This is a sample page"));
+            }
+        }
+    }
+
+    @Test
+    public void webTables() {
+        driver.get("https://demoqa.com/webtables");
+        driver.findElement(By.id(("edit-record-1"))).click();
+        driver.findElement(By.id(("firstName"))).clear();
+        driver.findElement(By.id(("firstName"))).sendKeys("Sabbir");
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Test
+    public void scrapData() {
+        driver.get("https://demoqa.com/webtables");
+        WebElement table = driver.findElement(By.className("rt-tbody"));
+        List<WebElement> allRows = table.findElements(By.className("rt-tr"));
+        int i = 0;
+        for (WebElement row : allRows) {
+            List<WebElement> cells = row.findElements(By.className("rt-td"));
+            for (WebElement cell : cells) {
+                i++;
+                System.out.println("num[" + i + "] " + cell.getText());
+            }
+        }
+    }
+
+    @Test
+    public void handleIframe() {
+        driver.get("https://demoqa.com/frames");
+        driver.switchTo().frame("frame2");
+        String text = driver.findElement(By.id("sampleHeading")).getText();
+        Assert.assertTrue(text.contains("This is a sample page"));
+        driver.switchTo().defaultContent();
+    }
+
 
     @After
     public void quit() throws InterruptedException {
